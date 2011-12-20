74ef0a1aa557575625054122b0bc09ef93ed6604
Ben Scott <ben@belfryimages.com.au>
2011-12-20 22:53:46 +1000

    got the current price moving the right way

--- stoploss.rb
+++ stoploss.rb
@@ -1,8 +1,21 @@
 class PriceChangedMessage
+	attr_reader :new_price
+
+	def initialize new_price
+		@new_price = new_price
+	end
 end
 
 class StopLoss
+	attr_reader :current_price
+
+	def initialize trail, current_price
+		@trail = trail
+		@current_price = current_price
+	end
+
 	def handle msg
+		@current_price = msg.new_price if msg.new_price > @current_price
 		:do_nothing
 	end
 end
--- stoploss_spec.rb
+++ stoploss_spec.rb
@@ -1,9 +1,27 @@
 require './stoploss.rb'
 
 describe StopLoss, '#handle' do
-	it 'does nothing for empty message' do
-		stoploss = StopLoss.new
-		action = stoploss.handle PriceChangedMessage.new
+	it 'does nothing when new price is same as current price' do
+		stoploss = StopLoss.new 1, 10
+		action = stoploss.handle PriceChangedMessage.new 10
 		action.should == :do_nothing
+	end
+end
+
+describe StopLoss, '#get_current_price' do
+	it 'stays the same when the new price is the same' do
+		stoploss = StopLoss.new 1, 10
+		stoploss.handle PriceChangedMessage.new 10
+		stoploss.current_price.should == 10
+	end
+	it 'stays the same when the new price is lower' do
+		stoploss = StopLoss.new 1, 10
+		stoploss.handle PriceChangedMessage.new 9
+		stoploss.current_price.should == 10
+	end
+	it 'is increased when the new price is higher' do
+		stoploss = StopLoss.new 1, 10
+		stoploss.handle PriceChangedMessage.new 11
+		stoploss.current_price.should == 11
 	end
 end
